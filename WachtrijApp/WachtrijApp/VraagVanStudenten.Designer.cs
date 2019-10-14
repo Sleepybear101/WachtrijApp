fa9c074b223446ec3316469257fb896114ac162b
Roy Heimel <Royheimel@hotmail.com>
2019-10-14 09:40:52 +0200

    kleine update

--- WachtrijApp/WachtrijApp/VraagVanStudenten.Designer.cs
+++ WachtrijApp/WachtrijApp/VraagVanStudenten.Designer.cs
@@ -49,7 +49,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(533, 71);
+            this.label1.Location = new System.Drawing.Point(630, 56);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(46, 17);
             this.label1.TabIndex = 1;
@@ -58,7 +58,7 @@
             // tbVraag
             // 
             this.tbVraag.Enabled = false;
-            this.tbVraag.Location = new System.Drawing.Point(645, 66);
+            this.tbVraag.Location = new System.Drawing.Point(769, 51);
             this.tbVraag.Name = "tbVraag";
             this.tbVraag.Size = new System.Drawing.Size(143, 22);
             this.tbVraag.TabIndex = 2;
@@ -66,7 +66,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(532, 99);
+            this.label2.Location = new System.Drawing.Point(629, 84);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(82, 17);
             this.label2.TabIndex = 3;
@@ -75,7 +75,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(532, 127);
+            this.label3.Location = new System.Drawing.Point(629, 112);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(130, 17);
             this.label3.TabIndex = 4;
@@ -83,7 +83,7 @@
             // 
             // btnOpgelost
             // 
-            this.btnOpgelost.Location = new System.Drawing.Point(713, 273);
+            this.btnOpgelost.Location = new System.Drawing.Point(824, 258);
             this.btnOpgelost.Name = "btnOpgelost";
             this.btnOpgelost.Size = new System.Drawing.Size(88, 33);
             this.btnOpgelost.TabIndex = 5;
@@ -93,7 +93,7 @@
             // 
             // btnArchiefOpenen
             // 
-            this.btnArchiefOpenen.Location = new System.Drawing.Point(536, 273);
+            this.btnArchiefOpenen.Location = new System.Drawing.Point(633, 258);
             this.btnArchiefOpenen.Name = "btnArchiefOpenen";
             this.btnArchiefOpenen.Size = new System.Drawing.Size(128, 31);
             this.btnArchiefOpenen.TabIndex = 6;
@@ -104,7 +104,7 @@
             // tbOnderwerp
             // 
             this.tbOnderwerp.Enabled = false;
-            this.tbOnderwerp.Location = new System.Drawing.Point(645, 94);
+            this.tbOnderwerp.Location = new System.Drawing.Point(769, 79);
             this.tbOnderwerp.Name = "tbOnderwerp";
             this.tbOnderwerp.Size = new System.Drawing.Size(143, 22);
             this.tbOnderwerp.TabIndex = 7;
@@ -112,7 +112,7 @@
             // lbGeholpenDoor
             // 
             this.lbGeholpenDoor.AutoSize = true;
-            this.lbGeholpenDoor.Location = new System.Drawing.Point(532, 156);
+            this.lbGeholpenDoor.Location = new System.Drawing.Point(629, 141);
             this.lbGeholpenDoor.Name = "lbGeholpenDoor";
             this.lbGeholpenDoor.Size = new System.Drawing.Size(107, 17);
             this.lbGeholpenDoor.TabIndex = 9;
@@ -120,7 +120,7 @@
             // 
             // tbGeholpenDoor
             // 
-            this.tbGeholpenDoor.Location = new System.Drawing.Point(645, 156);
+            this.tbGeholpenDoor.Location = new System.Drawing.Point(769, 141);
             this.tbGeholpenDoor.Name = "tbGeholpenDoor";
             this.tbGeholpenDoor.Size = new System.Drawing.Size(143, 22);
             this.tbGeholpenDoor.TabIndex = 10;
@@ -128,7 +128,7 @@
             // lbNotitie
             // 
             this.lbNotitie.AutoSize = true;
-            this.lbNotitie.Location = new System.Drawing.Point(532, 188);
+            this.lbNotitie.Location = new System.Drawing.Point(629, 173);
             this.lbNotitie.Name = "lbNotitie";
             this.lbNotitie.Size = new System.Drawing.Size(52, 17);
             this.lbNotitie.TabIndex = 11;
@@ -136,7 +136,7 @@
             // 
             // rtbNotities
             // 
-            this.rtbNotities.Location = new System.Drawing.Point(586, 188);
+            this.rtbNotities.Location = new System.Drawing.Point(698, 173);
             this.rtbNotities.Name = "rtbNotities";
             this.rtbNotities.Size = new System.Drawing.Size(214, 68);
             this.rtbNotities.TabIndex = 12;
@@ -145,7 +145,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(533, 40);
+            this.label6.Location = new System.Drawing.Point(630, 25);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(97, 17);
             this.label6.TabIndex = 13;
@@ -154,7 +154,7 @@
             // tbVolledig_naam
             // 
             this.tbVolledig_naam.Enabled = false;
-            this.tbVolledig_naam.Location = new System.Drawing.Point(645, 35);
+            this.tbVolledig_naam.Location = new System.Drawing.Point(769, 20);
             this.tbVolledig_naam.Name = "tbVolledig_naam";
             this.tbVolledig_naam.Size = new System.Drawing.Size(143, 22);
             this.tbVolledig_naam.TabIndex = 14;
@@ -167,20 +167,22 @@
             this.dtVraag.AllowUserToResizeRows = false;
             this.dtVraag.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
             this.dtVraag.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtVraag.Location = new System.Drawing.Point(13, 13);
+            this.dtVraag.Location = new System.Drawing.Point(12, 1);
+            this.dtVraag.MultiSelect = false;
             this.dtVraag.Name = "dtVraag";
             this.dtVraag.ReadOnly = true;
             this.dtVraag.RowHeadersWidth = 51;
             this.dtVraag.RowTemplate.Height = 24;
             this.dtVraag.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dtVraag.Size = new System.Drawing.Size(513, 290);
+            this.dtVraag.ShowEditingIcon = false;
+            this.dtVraag.Size = new System.Drawing.Size(604, 290);
             this.dtVraag.TabIndex = 16;
             this.dtVraag.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DtVraag_CellClick);
             // 
             // tbGevraagdDocent
             // 
             this.tbGevraagdDocent.Enabled = false;
-            this.tbGevraagdDocent.Location = new System.Drawing.Point(645, 122);
+            this.tbGevraagdDocent.Location = new System.Drawing.Point(769, 107);
             this.tbGevraagdDocent.Name = "tbGevraagdDocent";
             this.tbGevraagdDocent.Size = new System.Drawing.Size(143, 22);
             this.tbGevraagdDocent.TabIndex = 15;
@@ -190,7 +192,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(811, 315);
+            this.ClientSize = new System.Drawing.Size(922, 315);
             this.Controls.Add(this.dtVraag);
             this.Controls.Add(this.tbGevraagdDocent);
             this.Controls.Add(this.tbVolledig_naam);
