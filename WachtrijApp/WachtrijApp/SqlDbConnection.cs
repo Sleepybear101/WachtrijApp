ff22417048f33b41fd53002f802132efa15ee669
Roy Heimel <Royheimel@hotmail.com>
2019-10-29 09:13:33 +0100

    Update SqlDbConnection.cs

--- WachtrijApp/WachtrijApp/SqlDbConnection.cs
+++ WachtrijApp/WachtrijApp/SqlDbConnection.cs
@@ -19,7 +19,9 @@ namespace WachtrijApp
         {
             //_con = new MySqlConnection("Server=remotemysql.com; port=3306;  Database=sPjBjePdtf; username=sPjBjePdtf; password=DvLp0RVQAX ");
             _con = new MySqlConnection("Server=localhost;  Database=sPjBjePdtf; username=root; password= ");
+            _con.Close();
             _con.Open();
+               
         }
 
         public void SqlQuery(string queryText)
